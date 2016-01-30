df606955086fe48ab883583b9e473066200101ab
BenAychh <ben.tomas.hernandez@gmail.com>
2016-01-29 17:06:09 -0700

    Added delay to the input for history

--- beautifyinput.js
+++ beautifyinput.js
@@ -1,26 +1,39 @@
 var UpdateInput = {
-  delay: 150,        // delay after keystroke before updating
+  delay: 100,        // delay after keystroke before updating
   preview: null,     // filled in by Init below
   buffer: null,      // filled in by Init below
   timeout: null,     // store setTimout id
   mjRunning: false,  // true when MathJax is processing
   oldText: null,     // used to check if an update is needed
+  oldTextClean: null,
+  currentDiv: 0,
+  input: null,
+  timeoutRunning: false,
+  Running: function() {
+    return (this.mjRunning || this.timeoutRunning);
+  },
   Start: function () {
-    this.preview = document.getElementById("result");
+    var previews = document.getElementsByClassName("inputResult");
+    this.preview = previews[previews.length - 1];
     this.buffer = document.getElementById("buffer");
+    this.input = document.getElementById("expression");
   },
   Update: function () {
+    var previews = document.getElementsByClassName("inputResult");
+    this.preview = previews[previews.length - 1];
     if (this.timeout) {
       clearTimeout(this.timeout);
     }
     this.timeout = setTimeout(this.callback,this.delay);
+    this.timeoutRunning = true;
   },
-  CreatePreview: function () {
+  CreatePreview: function() {
     this.timeout = null;
     if (this.mjRunning) {
       return;
     }
-    var text = '`' + document.getElementById("expression").value + '`';
+    this.oldTextClean = document.getElementById("expression").value + '';
+    var text = '`' + this.oldTextClean + '`';
     if (text === this.oldtext) {
       return;
     }
@@ -32,10 +45,12 @@ var UpdateInput = {
     );
   },
   DrawMath: function () {
-    this.mjRunning = false;
     this.preview.innerHTML = this.buffer.innerHTML;
-  }
-
+    var theInput = this.oldtext.replace('`', '');
+    this.preview.setAttribute('data-input', this.oldTextClean);
+    this.mjRunning = false;
+    this.timeoutRunning = false;
+  },
 };
 UpdateInput.callback = MathJax.Callback(["CreatePreview",UpdateInput]);
 UpdateInput.callback.autoReset = true;  // make sure it can run more than once
