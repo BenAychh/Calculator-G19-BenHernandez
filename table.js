670af3d5d1070fad74015cb429be55acc147d002
BenAychh <ben.tomas.hernandez@gmail.com>
2016-02-02 18:25:36 -0700

    A working table canvas

--- table.js
+++ table.js
@@ -2,6 +2,7 @@ function Table(pCanvas) {
   var canvas = pCanvas;
   var context = canvas.getContext('2d');
   var lines = {};
+  var screenData = [];
   var startNumber = 1;
   var delta = 1;
   var maxFunctions = 4;
@@ -12,11 +13,24 @@ function Table(pCanvas) {
   var rightMargin = 150;
   var tableWidth = canvas.width - rightMargin;
   var headerColor = '#989898';
-  var headerFontColor = '#000000'
+  var headerFontColor = '#000000';
   var borderColor = '#000000';
   var xFontColor = '#000000';
+  var moveTimer;
   drawHeader();
   drawRows();
+  canvas.addEventListener("mousedown", getPosition, false);
+  function getPosition(event)
+  {
+    var x = event.x - 300;
+    var y = event.y;
+    var divideLines = tableWidth / maxFunctions;
+    if (x > 0 && y > headerHeight) {
+      var screenRow = math.floor((y - headerHeight) / rowHeight);
+      var screenColumn = math.floor(x / divideLines);
+      setInput( '(' + screenData[screenRow][screenColumn] + ')');
+    }
+  }
   this.repaint = function() {
     tableWidth = canvas.width - rightMargin;
     drawHeader();
@@ -31,16 +45,34 @@ function Table(pCanvas) {
     this.repaint();
   };
   this.moveDown = function() {
-    startNumber += delta;
-    drawRows();
-  }
+    intervalMoveDown();
+    moveTimer = setTimeout(function() {
+      repeat(intervalMoveDown);
+    }, 400);
+  };
+
   this.moveUp = function() {
-    startNumber -= delta;
-    drawRows();
-  }
+    intervalMoveUp();
+    moveTimer = setTimeout(function() {
+      repeat(intervalMoveUp);
+    }, 400);
+  };
+  this.stopMove = function() {
+    clearTimeout(moveTimer);
+  };
   this.setDelta = function(d) {
-    delta = d;
-    drawRows();
+    var evaluatedD = math.eval(d);
+    if (!isNaN(evaluatedD)) {
+      delta = evaluatedD;
+      drawRows();
+    }
+  }
+  this.setStart = function(s) {
+    var evaluatedS = math.eval(s);
+    if (!isNaN(evaluatedS)) {
+      startNumber = evaluatedS;
+      drawRows();
+    }
   }
   function drawHeader() {
     context.clearRect(0, 0, tableWidth, headerHeight);
@@ -80,7 +112,7 @@ function Table(pCanvas) {
   function drawRows() {
     context.clearRect(0, headerHeight, tableWidth, canvas.height - headerHeight);
     context.font = font + 'px Calibri';
-    var numberOfRows = math.ceil((tableWidth - headerHeight) / rowHeight);
+    var numberOfRows = math.ceil((canvas.height - headerHeight) / rowHeight);
     var columnWidth = tableWidth / maxFunctions;
     var centers = [];
     var divideLines = tableWidth / maxFunctions;
@@ -94,24 +126,50 @@ function Table(pCanvas) {
     context.fillRect(tableWidth - 3, headerHeight, 3, canvas.height - headerHeight);
     context.fillStyle = xFontColor;
     var keys = Object.keys(lines);
+    screenData = [];
     for (var j = 0; j <= numberOfRows; j++) {
-        var xNumber = math.round(j * delta + startNumber,3);
-        var dimensions = context.measureText(xNumber);
-        var bottomOfRow = headerHeight + (j + 1) * rowHeight;
-        var textTop = bottomOfRow - (rowHeight - font);
-        var textLeft = centers[0] - dimensions.width / 2;
-        context.fillStyle = xFontColor;
-        context.fillText(xNumber, textLeft, textTop);
-        context.fillStyle = borderColor;
-        context.fillRect(0, bottomOfRow - 1, tableWidth, 3);
-        for (var k = 0; k < keys.length; k++) {
-          var equation = lines[keys[k]].equation;
-          context.fillStyle = lines[keys[k]].color;
-          var functionNumber = math.round(math.eval(equation, {x: xNumber}), 3);
-          dimensions = context.measureText(functionNumber);
-          textLeft = centers[k + 1] - dimensions.width / 2;
-          context.fillText(functionNumber, textLeft, textTop);
+      var row = [];
+      var xNumber = math.round(j * delta + startNumber,3);
+      row.push(xNumber);
+      var dimensions = context.measureText(xNumber);
+      var bottomOfRow = headerHeight + (j + 1) * rowHeight;
+      var textTop = bottomOfRow - (rowHeight - font);
+      var textLeft = centers[0] - dimensions.width / 2;
+      context.fillStyle = xFontColor;
+      context.fillText(xNumber, textLeft, textTop);
+      context.fillStyle = borderColor;
+      context.fillRect(0, bottomOfRow - 1, tableWidth, 3);
+      for (var k = 0; k < keys.length; k++) {
+        var equation = lines[keys[k]].equation;
+        context.fillStyle = lines[keys[k]].color;
+        var functionNumber = math.eval(equation, {x: xNumber});
+        row.push(functionNumber);
+        if (!isNaN(functionNumber) && isFinite(functionNumber)) {
+          functionNumber = math.round(functionNumber, 4);
         }
+        dimensions = context.measureText(functionNumber.toLocaleString());
+        if (dimensions.width < divideLines) {
+          textLeft = centers[k + 1] - dimensions.width / 2;
+        } else {
+          textLeft = divideLines * (k + 1);
+        }
+        context.fillText(functionNumber.toLocaleString(), textLeft, textTop, divideLines);
+      }
+      screenData.push(row);
     }
+  }
+  function repeat(func) {
+    func();
+    moveTimer = setTimeout(function() {
+      repeat(func);
+    }, 100);
+  }
+  function intervalMoveDown() {
+    startNumber += delta;
+    drawRows();
+  }
+  function intervalMoveUp() {
+    startNumber -= delta;
+    drawRows();
   }
 }
