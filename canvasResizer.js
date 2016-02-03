e44eea344f2cbf7a835a42135ff22f79725172d9
BenAychh <ben.tomas.hernandez@gmail.com>
2016-02-02 18:24:36 -0700

    Updated to resize table canvas as well

--- canvasResizer.js
+++ canvasResizer.js
@@ -3,6 +3,7 @@ function resizeCanvas(){
   var info = document.getElementById("info");
   var table = document.getElementById("tables")
   var calculationHistory = document.getElementById("calculationHistory");
+  var container = document.getElementById('tableInterface');
   graphing.width = window.innerWidth -  300;
   graphing.height = window.innerHeight - 200;
   info.width = window.innerWidth -  300;
@@ -10,6 +11,7 @@ function resizeCanvas(){
   table.width = window.innerWidth -  300;
   table .height = window.innerHeight - 200;
   calculationHistory.setAttribute("style", "height: " + tables.height + "px");
+  container.setAttribute("style", "left: " + (tables.width - 150) + "px;")
   if (grapher) {
     grapher.repaint();
   }
